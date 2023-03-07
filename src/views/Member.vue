b4f41acf39b4448a9e73a695e6ab72703e6c6119
weijuer <weijuer@gmail.com>
2023-03-07 23:23:22 +0800

    2 files modified

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,21 +1,20 @@
 <script setup>
 import addMemberModal from './modal/AddMemberModal.vue'
 import { useMemberStore } from '../stores'
-import { NButton } from "naive-ui";
+import { NButton } from 'naive-ui'
 
 const showModal = ref(false)
-const { members, addMember, changeMember, removeMember } = useMemberStore()
+const source = ref({})
+const memberStore = useMemberStore()
 
-const createColumns = ({
-  changeMember, removeMember
-}) => {
+const createColumns = ({ onEditMember, onRemoveMember }) => {
   return [
     {
       title: '序号',
       key: 'no',
       render: (_, index) => {
         return index + 1
-      }
+      },
     },
     {
       title: '姓名',
@@ -25,32 +24,47 @@ const createColumns = ({
       title: '操作',
       key: 'actions',
       render(row) {
-        return [h(
-          NButton,
-          {
-            size: "small",
-            onClick: () => changeMember(row.id)
-          },
-          { default: () => "Edit" }
-        ),
-        h(
-          NButton,
-          {
-            size: "small",
-            onClick: () => removeMember(row.id)
-          },
-          { default: () => "Remove" }
-        )];
-      }
+        return [
+          h(
+            NButton,
+            {
+              size: 'small',
+              onClick: () => onEditMember(row),
+            },
+            { default: () => 'Edit' }
+          ),
+          h(
+            NButton,
+            {
+              size: 'small',
+              onClick: () => onRemoveMember(row.id),
+            },
+            { default: () => 'Remove' }
+          ),
+        ]
+      },
     },
   ]
 }
+
+const { changeMember, removeMember } = memberStore
+
+const onEditMember = (row) => {
+  showModal.value = true
+  source.value = row
+}
+
+const onRemoveMember = (row) => {
+  memberStore.removeMember(row.id)
+}
+
 const columns = createColumns({
-  changeMember, removeMember
+  onEditMember,
+  onRemoveMember,
 })
 
 const onPositiveClick = (model) => {
-  addMember(model.name)
+  memberStore.addMember(model.name)
   showModal.value = false
 }
 const onNegativeClick = () => {
@@ -64,9 +78,19 @@ const onNegativeClick = () => {
       新建
     </n-button>
   </n-space>
-  <n-data-table :columns="columns" :data="members" :bordered="false" />
-  <add-member-modal v-model:show="showModal" positive-text="确认" negative-text="取消" @positive-click="onPositiveClick"
-    @negative-click="onNegativeClick" />
+  <n-data-table
+    :columns="columns"
+    :data="memberStore.members"
+    :bordered="false"
+  />
+  <add-member-modal
+    :source="source"
+    v-model:show="showModal"
+    positive-text="确认"
+    negative-text="取消"
+    @positive-click="onPositiveClick"
+    @negative-click="onNegativeClick"
+  />
 </template>
 
 <style lang="scss" scoped>
--- src/views/modal/AddMemberModal.vue
+++ src/views/modal/AddMemberModal.vue
@@ -1,7 +1,10 @@
 <script setup>
-const props = defineProps({})
+const props = defineProps({
+  source: Object,
+})
 const emit = defineEmits(['negative-click', 'positive-click'])
 
+const title = computed(() => (props.source.id !== 0 ? '编辑' : '新建'))
 const formRef = ref()
 const formModel = reactive({
   name: null,
@@ -16,11 +19,12 @@ const rules = {
 }
 
 const onPositiveHandle = (e) => {
-  e.preventDefault();
+  e.preventDefault()
   formRef.value?.validate((errors) => {
     if (!errors) {
-      emit('positive-click', formModel.value)
-      formModel.value.name = ''
+      console.log(formModel)
+      emit('positive-click', formModel)
+      formModel.name = ''
       console.log('Valid')
     } else {
       console.log(errors)
@@ -30,22 +34,32 @@ const onPositiveHandle = (e) => {
 </script>
 
 <template>
-  <n-modal v-bind="$attrs" :show-icon="false" preset="dialog" title="新建">
-    <n-form class="w-form" ref="formRef" :model="formModel" :rules="rules" label-placement="left" :label-width="80"
-      require-mark-placement="right-hanging" size="medium">
+  <n-modal v-bind="$attrs" :show-icon="false" preset="dialog" :title="title">
+    <n-form
+      class="w-form"
+      ref="formRef"
+      :model="formModel"
+      :rules="rules"
+      label-placement="left"
+      :label-width="80"
+      require-mark-placement="right-hanging"
+      size="medium"
+    >
       <n-form-item label="姓名" path="name">
         <n-input v-model:value="formModel.name" placeholder="请输入姓名" />
       </n-form-item>
       <n-form-item label="备注" path="extra">
-        <n-input type="textarea" v-model:value="formModel.extra" placeholder="请输入备注" />
+        <n-input
+          type="textarea"
+          v-model:value="formModel.extra"
+          placeholder="请输入备注"
+        />
       </n-form-item>
     </n-form>
 
     <template #action>
       <n-button @click="emit('negative-click')">取消</n-button>
-      <n-button @click="onPositiveHandle" type="info">
-        确认
-      </n-button>
+      <n-button @click="onPositiveHandle" type="info"> 确认 </n-button>
     </template>
   </n-modal>
 </template>
